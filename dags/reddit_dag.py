faf55e7202b69b71de5b44d637d2cd79724c27fb
Márcio Pereira <marciopereira97@hotmail.com>
2025-03-28 16:40:11 +0000

    updated

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -10,39 +10,34 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
-default_args = {
-    'owner': 'Yusuf Ganiyu',
-    'start_date': datetime(2023, 10, 22)
-}
+default_args = {"owner": "marciofgpereira", "start_date": datetime(2025, 3, 28)}
 
 file_postfix = datetime.now().strftime("%Y%m%d")
 
 dag = DAG(
-    dag_id='etl_reddit_pipeline',
+    dag_id="etl_reddit_pipeline",
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule_interval="@daily",
     catchup=False,
-    tags=['reddit', 'etl', 'pipeline']
+    tags=["reddit", "etl", "pipeline"],
 )
 
 # extraction from reddit
 extract = PythonOperator(
-    task_id='reddit_extraction',
+    task_id="reddit_extraction",
     python_callable=reddit_pipeline,
     op_kwargs={
-        'file_name': f'reddit_{file_postfix}',
-        'subreddit': 'dataengineering',
-        'time_filter': 'day',
-        'limit': 100
+        "file_name": f"reddit_{file_postfix}",
+        "subreddit": "dataengineering",
+        "time_filter": "week",
+        "limit": 100,
     },
-    dag=dag
+    dag=dag,
 )
 
 # upload to s3
 upload_s3 = PythonOperator(
-    task_id='s3_upload',
-    python_callable=upload_s3_pipeline,
-    dag=dag
+    task_id="s3_upload", python_callable=upload_s3_pipeline, dag=dag
 )
 
 extract >> upload_s3
--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -10,9 +10,9 @@ from utils.constants import POST_FIELDS
 
 def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     try:
-        reddit = praw.Reddit(client_id=client_id,
-                             client_secret=client_secret,
-                             user_agent=user_agent)
+        reddit = praw.Reddit(
+            client_id=client_id, client_secret=client_secret, user_agent=user_agent
+        )
         print("connected to reddit!")
         return reddit
     except Exception as e:
@@ -20,7 +20,9 @@ def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
         sys.exit(1)
 
 
-def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None):
+def extract_posts(
+    reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None
+):
     subreddit = reddit_instance.subreddit(subreddit)
     posts = subreddit.top(time_filter=time_filter, limit=limit)
 
@@ -35,15 +37,16 @@ def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, lim
 
 
 def transform_data(post_df: pd.DataFrame):
-    post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
-    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
-    post_df['author'] = post_df['author'].astype(str)
-    edited_mode = post_df['edited'].mode()
-    post_df['edited'] = np.where(post_df['edited'].isin([True, False]),
-                                 post_df['edited'], edited_mode).astype(bool)
-    post_df['num_comments'] = post_df['num_comments'].astype(int)
-    post_df['score'] = post_df['score'].astype(int)
-    post_df['title'] = post_df['title'].astype(str)
+    post_df["created_utc"] = pd.to_datetime(post_df["created_utc"], unit="s")
+    post_df["over_18"] = np.where(post_df["over_18"], True, False)
+    post_df["author"] = post_df["author"].astype(str)
+    edited_mode = post_df["edited"].mode()
+    post_df["edited"] = np.where(
+        post_df["edited"].isin([True, False]), post_df["edited"], edited_mode
+    ).astype(bool)
+    post_df["num_comments"] = post_df["num_comments"].astype(int)
+    post_df["score"] = post_df["score"].astype(int)
+    post_df["title"] = post_df["title"].astype(str)
 
     return post_df
 
